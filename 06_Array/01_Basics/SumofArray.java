330a9fb2f4884af7b94fdff695e73318fad8b263
Vishal Kumar Pal <mr.vishalkpal@gmail.com>
2025-09-25 15:32:08 +0530

    Add SumofArray.java file in 06_Array inside of 01_basics

---
+++ 06_Array/01_Basics/SumofArray.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+public class SumofArray {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a size of array: ");
+        int size = sc.nextInt();
+
+        int numbers[] = new int[size];
+        System.out.print("Enter " + size + " elements: ");
+        int sum = 0;
+        for(int i = 0; i < size; i++){
+            numbers[i] = sc.nextInt();
+            sum = numbers[i] + sum;
+        }
+
+        System.out.println("Sum of arrays: " + sum);
+
+        sc.close();
+
+    }
+}
