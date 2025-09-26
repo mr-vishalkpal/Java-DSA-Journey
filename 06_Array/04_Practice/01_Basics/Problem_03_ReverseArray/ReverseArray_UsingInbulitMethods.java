6fe1bd942846bcc94cc76e4650e9a49dcfc9f3c1
Vishal Kumar Pal <mr.vishalkpal@gmail.com>
2025-09-27 00:11:13 +0530

    add ReverseArray_UsingInbuiltMethods.java in Problem_03_ReverseArray

---
+++ 06_Array/04_Practice/01_Basics/Problem_03_ReverseArray/ReverseArray_UsingInbulitMethods.java
@@ -0,0 +1,26 @@
+package Problem_03_ReverseArray;
+
+// Problem: Revrse array
+// Solution: Using Inbuilt Methods
+
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class ReverseArray_UsingInbulitMethods {
+
+    // function to reverse an array
+    static void reverseArray(List<Integer> arr){
+        Collections.reverse(arr);
+    }
+
+    public static void main(String args[]){
+        List<Integer> arr = new ArrayList<>(Arrays.asList(1, 4, 3, 2, 6, 5));
+    reverseArray(arr);
+    for(int i = 0; i < arr.size(); i++){
+        System.out.print(arr.get(i) +" ");
+    }
+    }
+    
+}
