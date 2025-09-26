8617c3e3e84377b7a4354a944e972ad0a144287f
Vishal Kumar Pal <mr.vishalkpal@gmail.com>
2025-09-26 07:12:39 +0530

    Add file in sode practice

--- 06_Array/04_Practice/01_Basics/Problem_01_Second_Largest_Element/SecondLargest_Brute.java
+++ 06_Array/04_Practice/01_Basics/Problem_01_Second_Largest_Element/SecondLargest_Brute.java
@@ -1,46 +1,50 @@
-// Probelm: Given an array of positive integers arr[] of size n, the task is to find second largest distinct element in the array.
-// Solution: Burteforce Approcach
+// Problem: Given an array of positive integers arr[] of size n, 
+// the task is to find second largest distinct element in the array.
+// Solution: Brute force approach
 // Date: 26-Sep-2025
 
 import java.util.Scanner;
+
 public class SecondLargest_Brute {
     public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter a size of Arrays: ");
-        int size = sc.nextInt();
+        try (Scanner sc = new Scanner(System.in)) {   // 
 
-        int numbers[] = new int[size];
-        System.out.print("Enter " + size + " elements: ");
-        
-        for(int i = 0; i < size; i++){
-            numbers[i] = sc.nextInt();
-        }
+            System.out.print("Enter a size of Arrays: ");
+            int size = sc.nextInt();
 
-        if(size < 2){
-            System.out.println("Array must have at least 2 elements.");
-            return;
-        }
+            int numbers[] = new int[size];
+            System.out.print("Enter " + size + " elements: ");
 
-        for(int i = 0; i < size - 1; i++){
-            for(int j = 0; j < size - 1 - i; j++){
-                if(numbers[j] > numbers[j+1]){
-                    int temp = numbers[j];
-                    numbers[j] = numbers[j+1];
-                    numbers[j+1] = temp;
-                }
+            for (int i = 0; i < size; i++) {
+                numbers[i] = sc.nextInt();
             }
-        }
-        for(int i = size - 2; i >= 0; i--){
-            if(numbers[i] != numbers[size - 1]){
-                System.out.println("Second largest element is: " + numbers[i]);
+
+            if (size < 2) {
+                System.out.println("Array must have at least 2 elements.");
                 return;
             }
-        }
-        
 
-        System.out.println("No second largest element found.");
-        sc.close();
-        
-    }    
+            // Bubble sort
+            for (int i = 0; i < size - 1; i++) {
+                for (int j = 0; j < size - 1 - i; j++) {
+                    if (numbers[j] > numbers[j + 1]) {
+                        int temp = numbers[j];
+                        numbers[j] = numbers[j + 1];
+                        numbers[j + 1] = temp;
+                    }
+                }
+            }
+
+            // Find second largest distinct
+            for (int i = size - 2; i >= 0; i--) {
+                if (numbers[i] != numbers[size - 1]) {
+                    System.out.println("Second largest element is: " + numbers[i]);
+                    return;
+                }
+            }
+
+            System.out.println("No second largest element found.");
+        }
+    }
 }
