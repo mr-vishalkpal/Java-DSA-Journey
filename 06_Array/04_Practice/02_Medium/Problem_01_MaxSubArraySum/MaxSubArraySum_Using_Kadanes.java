54bcda527d19d5a55f653f6964309a5b7fd6fa0f
Vishal Kumar Pal <mr.vishalkpal@gmail.com>
2025-09-27 22:00:44 +0530

    Add for cs

--- 06_Array/04_Practice/02_Medium/Problem_01_MaxSubArraySum/MaxSubArraySum_Using_Kadanes.java
+++ 06_Array/04_Practice/02_Medium/Problem_01_MaxSubArraySum/MaxSubArraySum_Using_Kadanes.java
@@ -9,10 +9,7 @@ public class MaxSubArraySum_Using_Kadanes {
         int cs = 0;
 
         for(int i = 0; i < n; i++){
-            cs += arr[i];
-            if(cs < 0){
-                cs = 0;
-            }
+            cs = Math.max(arr[i], cs + arr[i]);
             ms = Math.max(cs , ms);
         }
         return ms;
