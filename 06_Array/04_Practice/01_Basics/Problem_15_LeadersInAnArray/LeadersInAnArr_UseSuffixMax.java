585d4842330643fe6442434914d43fa3e4a9eef5
Vishal Kumar Pal <mr.vishalkpal@gmail.com>
2026-03-08 15:20:57 +0530

    Add problem 15 second Approach using Suffix maximum

---
+++ 06_Array/04_Practice/01_Basics/Problem_15_LeadersInAnArray/LeadersInAnArr_UseSuffixMax.java
@@ -0,0 +1,45 @@
+// Problem: Given an array arr[] of size n, the task is to find all the Leaders in the array. An element is a Leader if it is greater than or equal to all the elements to its right side.
+// Solution: Using Suffix Maximum
+// Date: 08-03-2026
+// Author: Vishal Kumar
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+
+public class LeadersInAnArr_UseSuffixMax {
+    
+    // Function to find the leaders in an array
+    static ArrayList<Integer> leaders(int arr[]){
+        ArrayList<Integer> result = new ArrayList<>();
+        int n = arr.length;
+
+        // Start with the rightmost element
+        int maxRight = arr[n - 1];
+
+        // Rightmost element is always a leader
+        result.add(maxRight);
+
+        // Traverse the array from rigth to left
+        for(int i = n - 2; i >= 0; i--){
+            if(arr[i] >= maxRight){
+                maxRight = arr[i];
+                result.add(maxRight);
+            }
+        }
+
+        // Reverse the result list to maintain original order
+        Collections.reverse(result);
+
+        return result;
+    }
+
+    public static void main(String args[]) {
+        int arr[] = {16, 17, 4, 3, 5, 2};
+        ArrayList<Integer> result = leaders(arr);
+        for(int res : result){
+            System.out.print(res + " ");
+        }
+        System.out.println();
+    }
+}
