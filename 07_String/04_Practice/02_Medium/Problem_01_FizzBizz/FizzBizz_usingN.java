f17411fac8df2a1fbba9b7ab1b9059172d21eae3
Vishal Kumar Pal <mr.vishalkpal@gmail.com>
2026-04-20 22:20:09 +0530

    Add problem of medium fizz bizz

---
+++ 07_String/04_Practice/02_Medium/Problem_01_FizzBizz/FizzBizz_usingN.java
@@ -0,0 +1,55 @@
+// Problem: Given an integer n, for every positive integer i <= n, the task is to print,
+// Solution: Using Checking every Integer
+// Date: 20-04-2026
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FizzBizz_usingN {
+    
+
+    static ArrayList<String> fizzBuzz(int n){
+      
+        ArrayList<String> res = new ArrayList<>();
+
+        for (int i = 1; i <= n; ++i) {
+          
+            // Check if i is divisible by both 3 and 5
+            if (i % 3 == 0 && i % 5 == 0) {
+              
+                // Add "FizzBuzz" to the result list
+                res.add("FizzBuzz");
+            }
+          
+            // Check if i is divisible by 3
+            else if (i % 3 == 0) {
+              
+                // Add "Fizz" to the result list
+                res.add("Fizz");
+            }
+          
+            // Check if i is divisible by 5
+            else if (i % 5 == 0) {
+              
+                // Add "Buzz" to the result list
+                res.add("Buzz");
+            }
+            else {
+              
+                // Add the current number as a string to the
+                // result list
+                res.add(Integer.toString(i));
+            }
+        }
+
+        return res;
+    }
+
+    public static void main(String[] args){
+        int n = 20;
+        ArrayList<String> res = fizzBuzz(n);
+        for (String s : res) {
+            System.out.print(s + " ");
+        }
+    }
+}
