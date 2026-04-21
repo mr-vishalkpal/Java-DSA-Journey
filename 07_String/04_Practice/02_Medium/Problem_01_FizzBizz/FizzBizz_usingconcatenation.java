2478789d7ade0925e718bc220fe11ce3d218bbc8
Vishal Kumar Pal <mr.vishalkpal@gmail.com>
2026-04-21 21:09:01 +0530

    Add problem of second appraoch

---
+++ 07_String/04_Practice/02_Medium/Problem_01_FizzBizz/FizzBizz_usingconcatenation.java
@@ -0,0 +1,41 @@
+// Problem: Given an integer n, for every positive integer i <= n, the task is to print,
+// Solution: Using Checking every Integer
+// Date: 21-04-2026
+
+import java.util.ArrayList;
+
+public class FizzBizz_usingconcatenation {
+    
+        static ArrayList<String> fizzBuzz(int n) {
+        ArrayList<String> res = new ArrayList<>();
+
+        for (int i = 1; i <= n; i++) {
+          
+           // Initialize an empty string for the current result
+            String s = ""; 
+
+            // Divides by 3, add Fizz
+            if (i % 3 == 0) s += "Fizz";
+          
+            // Divides by 5, add Buzz
+            if (i % 5 == 0) s += "Buzz";
+          
+            // Not divisible by 3 or 5, add the number
+            if (s.isEmpty()) s += i;
+			
+            // Append the current result to the list
+            res.add(s);
+        }
+
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int n = 20; 
+        ArrayList<String> res = fizzBuzz(n);
+
+        for (String s : res) {
+            System.out.print(s + " ");
+        }
+    }
+}
