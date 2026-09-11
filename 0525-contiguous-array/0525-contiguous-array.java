050341d6ec1e8b9f28ca6dad30659afe99a3ac4d
mr-vishalkpal <mr.vishalkpal@gmail.com>
2026-09-11 23:06:24 +0530

    Time: 24 ms (53.06%), Space: 65.7 MB (25.1%) - LeetHub

---
+++ 0525-contiguous-array/0525-contiguous-array.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int findMaxLength(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        map.put(0, -1);
+
+        int prefixSum = 0;
+        int maxLength = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+
+            if (nums[i] == 0) {
+                prefixSum--;
+            } else {
+                prefixSum++;
+            }
+
+            if (map.containsKey(prefixSum)) {
+                int length = i - map.get(prefixSum);
+                maxLength = Math.max(maxLength, length);
+            } else {
+                map.put(prefixSum, i);
+            }
+        }
+
+        return maxLength;
+    }
+}
